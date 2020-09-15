037110bdda61ae2bec85edc695ce47e57eaa304f
train <train@TRN0561.dmztrn.amdocs.trn>
2020-09-15 20:04:09 +0530

    first push of maven project.

---
+++ src/main/java/amdocs_edureka/mavnex/ExampleMVN.java
@@ -0,0 +1,10 @@
+package amdocs_edureka.mavnex;
+
+public class ExampleMVN 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "This is an example for testing STS, Maven and Git integration." );
+    }
+}
